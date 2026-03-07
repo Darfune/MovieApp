b3d2d0fc4af6ea8b7335614985c3450b834de02a
JasonCharalampidis <jcharalampidis@novibet.com>
2026-03-07 11:56:49 +0200

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Jason Charalampidis on 7/3/26.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MovieAppTests/MovieAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppTests.swift
+//  MovieAppTests
+//
+//  Created by Jason Charalampidis on 7/3/26.
+//
+
+import Testing
+@testable import MovieApp
+
+struct MovieAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
